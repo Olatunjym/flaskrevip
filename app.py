c7e194f1f53d93c197f793b2d4003dcd5f22c58a
Olatunji <adeteyelinux@gmail.com>
2024-06-27 18:14:01 +0100

    updating the cicd to reflect the final change

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+def get_client_ip():
+    if request.headers.get('X-Forwarded-For'):
+        client_ip = request.headers.get('X-Forwarded-For').split(',')[0]
+    else:
+        client_ip = request.remote_addr
+    return client_ip
+
 @app.route('/')
 def reverse_ip():
-    client_ip = request.remote_addr
+    client_ip = get_client_ip()
     reversed_ip = '.'.join(client_ip.split('.')[::-1])
-    return reversed_ip
+    return f"Reversed IP: {reversed_ip}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
